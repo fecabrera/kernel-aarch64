1dab98e7c4667f9d01da5d794d46b00ff95dd7cc
Felipe Cabrera <fecabrera@protonmail.com>
2026-05-24 22:18:31 -0400

    feat: integrate heap and memory register into kernel_main

--- src/kernel.c
+++ src/kernel.c
@@ -5,6 +5,7 @@
 #include "timer.h"
 #include "dtb.h"
 #include "rtc.h"
+#include "heap.h"
 
 extern void *dtb_ptr;
 
@@ -12,6 +13,23 @@ void kernel_main()
 {
     dtb_init(dtb_ptr);
     dtb_dump();
+
+    struct memreg mem;
+    if (dtb_get_memory_register(&mem) == 0)
+    {
+        uart_puts("Memory base: 0x");
+        uart_put_uint_base(mem.base, 16);
+        uart_puts("\r\n");
+
+        uart_puts("Memory size: ");
+        uart_put_uint(mem.size / (1024 * 1024));
+        uart_puts(" MiB");
+        uart_puts("\r\n");
+    }
+    else
+    {
+        uart_puts("Memory register not found!");
+    }
 
     gic_init();
     interrupts_init();
@@ -21,6 +39,8 @@ void kernel_main()
 
     timer_set_interval(10);
     irq_enable();
+
+    heap_init();
 
     uint32_t timestamp = rtc_get_time();
     uart_puts("Current timestamp: ");
